35d6032b8f10cd4f5be320878fb940bad79a0edc
fendouai <fendouai@gmail.com>
2016-09-06 15:55:18 +0800

    fix the venv

--- autobuild.py
+++ autobuild.py
@@ -9,30 +9,33 @@ def git_pull():
     logging.info(commands.getstatusoutput("git pull origin renew"))
     print ("git pull origin renew")
 
+'''
 def set_venv():
     print (os.chdir("/opt/push/jpush-docs/"))
     logging.info (commands.getstatusoutput(". venv/bin/activate"))
     print (". venv/bin/activate")
+'''
+
 
 def build():
     print (os.chdir("/opt/push/jpush-docs/jpush-docs/zh/JPush/"))
     print ("JPush/")
-    logging.info (commands.getstatusoutput("mkdocs build"))
+    logging.info (commands.getstatusoutput("/opt/push/jpush-docs/venv/bin/mkdocs build"))
     time.sleep(1)
     print time.asctime(time.localtime(time.time()))
     logging.info (os.chdir("/opt/push/jpush-docs/jpush-docs/zh/JMessage/"))
     print ("JMessage/")
-    logging.info (commands.getstatusoutput("mkdocs build"))
+    logging.info (commands.getstatusoutput("/opt/push/jpush-docs/venv/bin/mkdocs build"))
     time.sleep(1)
     print time.asctime(time.localtime(time.time()))
     logging.info (os.chdir("/opt/push/jpush-docs/jpush-docs/zh/JSMS/"))
     print ("JSMS/")
-    logging.info (commands.getstatusoutput("mkdocs build"))
+    logging.info (commands.getstatusoutput("/opt/push/jpush-docs/venv/bin/mkdocs build"))
     time.sleep(1)
     print time.asctime(time.localtime(time.time()))
     logging.info (os.chdir("/opt/push/jpush-docs/jpush-docs/zh/Index/"))
     print ("Index/")
-    logging.info (commands.getstatusoutput("mkdocs build"))
+    logging.info (commands.getstatusoutput("/opt/push/jpush-docs/venv/bin/mkdocs build"))
     time.sleep(1)
     print time.asctime(time.localtime(time.time()))
 
@@ -42,7 +45,10 @@ logging.basicConfig(level=logging.DEBUG,
                     datefmt='%a, %d %b %Y %H:%M:%S',
                     filename='/opt/push/jpush-docs/autobuild.log',
                     filemode='w')
+'''
 set_venv()
+'''
+
 git_pull()
 build()
 print time.asctime(time.localtime(time.time()))
