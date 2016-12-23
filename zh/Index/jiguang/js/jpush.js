bda7f22eaf6bfcaf82e3223c5c823d5de42be4ed
liyuan <liyuan1@jpush.com>
2016-12-23 16:38:47 +0800

    modify style compatible mobile phone

--- zh/Index/jiguang/js/jpush.js
+++ zh/Index/jiguang/js/jpush.js
@@ -2,38 +2,9 @@
 function setPageSize() {
     var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     if (w < 800) {
-        $(".header").hide();
-        $("#rtd-search-form").hide();
-        $(“.top-title”).css({“margin-top”: “90px”, “margin-bottom”: “-32px”});
-        $("html").css({"overflow-x":"hidden", "width":w+"px"});
-        if ($("#phone-head-menu").length <= 0) {
-            var menu = ‘\
-            <select id="phone-head-menu" onchange=mbar(this) style="position: absolute; top: 14px; right: 6px;font-weight: 600; -webkit-box-shadow: 0 3px 0 #ccc,0 -1px #fff inset; -moz-box-shadow: 0 3px 0 #ccc,0 -1px #fff inset; box-shadow: 0 3px 0 #ccc,0 -1px #fff inset; background: #1b75bb; color: #fff; font-size: 14px; border: none; outline: none;display: inline;-webkit-appearance: none;-moz-appearance: none;cursor: pointer;-webkit-border-radius: 0px;-moz-border-radius: 0px;border-radius: 0px;width: 62px;">\
-                <option>菜单 +</option>\
-                <option value="/jpush/guideline/intro/">JPush</option>\
-                <option value="/janalytics/guideline/intro/">JAnalytics</option>\
-                <option value="/jmessage/guideline/jmessage_guide/">JMessage</option>\
-                <option value="/jsms/guideline/JSMS_guide/">JSMS</option>\
-            </select>';
-            $(".container-fluid").append(menu);
-        }
+        $("#top-title").css({"margin-top":30+"px"});
     } else {
-        $(".header").show();
-        $("#rtd-search-form").show();
-        $("html").removeAttr("style");
-        if ($("#phone-head-menu").length > 0) {
-            $("#phone-head-menu").remove();
-        }
-    }
-}
 
-// 手机端菜单选择
-function mbar(sobj) {
-    var docurl = sobj.options[sobj.selectedIndex].value;
-    if (docurl != "") {
-        open(docurl, '_self');
-        sobj.selectedIndex = 0;
-        sobj.blur();
     }
 }
 
