296faa06873e9c376e44708cdcd050029cd9fa69
Helperhaps <helperhaps@users.noreply.github.com>
2017-09-12 11:05:43 +0000

    format log

--- hooks/webhooks.py
+++ hooks/webhooks.py
@@ -21,7 +21,13 @@ REPOS = [
     'jsms-api-csharp-client'
 ]
 current = os.path.split(os.path.realpath(__file__))[0]
+separator = "\n========================================================================\n"
 log_file = "{0}/hooks.log".format(current)
+
+def log_it(str):
+    f = open(log_file,'a')
+    f.write(str)
+    f.close()
 
 @app.route("/")        # Standard Flask endpoint
 def hello_world():
@@ -29,25 +35,24 @@ def hello_world():
 
 @webhook.hook('ping')    # Defines a handler for the 'ping' event
 def on_ping(data):
-    print('pong')
-
+    log_it(separator + "Pong Pong\n\n")
 
 @webhook.hook()        # Defines a handler for the 'push' event
 def on_push(data):
     branch = data['ref'].split('/', 2)[2];
     repo = data['repository']['name']
-
-    app.logger.info('An error occurred')
+    greeting = separator + "Received Push Event From {repo}#{branch}..\n\n".format(repo=repo, branch=branch)
 
     if (repo == 'jpush-docs'):
-        result = os.popen("{0}/depoly.sh {1}".format(current, branch))
-    if (branch == 'master' and repo in REPOS):
-        result = os.popen("{0}/synreadme.sh {1}".format(current, repo))
+        f = os.popen("{0}/depoly.sh {1}".format(current, branch))
+        result = f.read()
+    elif (branch == 'master' and repo in REPOS):
+        f = os.popen("{0}/synreadme.sh {1}".format(current, repo))
+        result = f.read()
+    else:
+        result = "But we will not handle it"
 
-    f = open(log_file,'a')
-    f.write(result.read())
-    f.close()
-
+    log_it(greeting + result + "\n\n")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080)
